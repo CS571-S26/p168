cea4ea6667e27bcb05d55bac4acc9640e68a4058
adi <menon27@wisc.edu>
2026-03-18 16:48:21 -0500

    step 1

---
+++ p168-local/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
+      <h1>p168</h1>
+      <p>This is my initial React deployment.</p>
+    </div>
+  );
+}
+
+export default App;
